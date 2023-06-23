5aca94577bfcfb6774ba06d5f337c02a2674bb74
Yoon-Min <yoonmin0113@naver.com>
2023-06-24 07:35:49 +0900

    [LAYOUT] 로그인 프로토타입 제작

---
+++ app/src/main/java/com/example/protalktime/presentation/sign/SignInFragment.kt
@@ -0,0 +1,22 @@
+package com.example.protalktime.presentation.sign
+
+import android.os.Bundle
+import android.view.View
+import com.example.protalktime.R
+import com.example.protalktime.databinding.FragmentSignInBinding
+import com.example.protalktime.presentation.common.BaseFragment
+import com.example.protalktime.presentation.common.NavigationUtil.navigate
+import com.example.protalktime.presentation.common.NavigationUtil.navigateUp
+import com.example.protalktime.util.Constants
+
+class SignInFragment : BaseFragment<FragmentSignInBinding>(R.layout.fragment_sign_in) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.btnLogin.setOnClickListener {
+            Constants.userInfo.authorization = binding.etId.text.toString()
+            navigate(R.id.action_signInFragment_to_meetingMainFragment)
+        }
+    }
+}
