2a51485c3a9001fa17a034e22b109c01e72e8d3d
Yoon-Min <yoonmin0113@naver.com>
2023-06-24 00:44:26 +0900

    [LAYOUT] 매칭 메인 페이지 제작

--- app/src/main/java/com/example/protalktime/presentation/main/MatchingMainFragment.kt
+++ app/src/main/java/com/example/protalktime/presentation/main/MatchingMainFragment.kt
@@ -6,6 +6,10 @@ import com.example.protalktime.R
 import com.example.protalktime.databinding.FragmentMeetingMainBinding
 import com.example.protalktime.presentation.common.BaseFragment
 import com.example.protalktime.presentation.common.WindowUtil.setStatusBarColor
+import com.example.protalktime.util.Constants
+import com.google.android.material.chip.Chip
+import com.google.android.material.tabs.TabLayout
+import timber.log.Timber
 
 class MatchingMainFragment : BaseFragment<FragmentMeetingMainBinding>(R.layout.fragment_meeting_main) {
 
@@ -13,6 +17,48 @@ class MatchingMainFragment : BaseFragment<FragmentMeetingMainBinding>(R.layout.f
         super.onViewCreated(view, savedInstanceState)
 
         setStatusBarColor(R.color.transparent)
+
+        setChipGroup("교내")
+        handleSelectedLocationOption()
     }
 
+    private fun handleSelectedLocationOption() {
+        binding.tlOptionLocation.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+                Timber.d("선택된 지역 옵션 : ${tab?.text}")
+                setChipGroup(tab?.text.toString())
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab?) {
+            }
+
+            override fun onTabReselected(tab: TabLayout.Tab?) {
+            }
+        })
+    }
+
+    private fun setChipGroup(selectedLocation: String) {
+        binding.cgSearchCategoryGroup.removeAllViews()
+        var targetList = listOf<String>()
+        when (selectedLocation) {
+            "교내" -> {
+                targetList = Constants.chipSetByInUniversity
+            }
+
+            "교외" -> {
+                targetList = Constants.chipSetByOutside
+            }
+        }
+
+        for (category in targetList) {
+            val chip: Chip = layoutInflater.inflate(
+                R.layout.chip_gray_background,
+                binding.cgSearchCategoryGroup,
+                false
+            ) as Chip
+            binding.cgSearchCategoryGroup.addView(chip.apply {
+                text = category
+            })
+        }
+    }
 }
---
+++ app/src/main/java/com/example/protalktime/util/Constants.kt
@@ -0,0 +1,15 @@
+package com.example.protalktime.util
+
+object Constants {
+
+    val chipSetByInUniversity = listOf(
+        "후생관",
+        "기숙사",
+        "어문관"
+    )
+
+    val chipSetByOutside = listOf(
+        "외대 사거리",
+        "모현 사거리"
+    )
+}
