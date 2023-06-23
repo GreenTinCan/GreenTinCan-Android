82b7024b3a184f23e2c44f570fe2cf12b988c833
Yoon-Min <yoonmin0113@naver.com>
2023-06-24 08:05:44 +0900

    [LAYOUT] 리뷰 페이지 제작

---
+++ app/src/main/java/com/example/protalktime/presentation/main/ReviewFragment.kt
@@ -0,0 +1,19 @@
+package com.example.protalktime.presentation.main
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.protalktime.R
+import com.example.protalktime.databinding.FragmentReviewBinding
+import com.example.protalktime.presentation.common.BaseFragment
+
+class ReviewFragment : BaseFragment<FragmentReviewBinding>(R.layout.fragment_review) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+
+}
