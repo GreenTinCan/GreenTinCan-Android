1c55014b7703001c5fc46ca73e08d827ea6bc99d
Yoon-Min <yoonmin0113@naver.com>
2023-06-24 08:21:09 +0900

    [LAYOUT] 리뷰 페이지 제작

--- app/src/main/java/com/example/protalktime/presentation/main/ReviewFragment.kt
+++ app/src/main/java/com/example/protalktime/presentation/main/ReviewFragment.kt
@@ -5,15 +5,40 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.core.content.ContextCompat
 import com.example.protalktime.R
 import com.example.protalktime.databinding.FragmentReviewBinding
 import com.example.protalktime.presentation.common.BaseFragment
 
 class ReviewFragment : BaseFragment<FragmentReviewBinding>(R.layout.fragment_review) {
 
+    private var goodClick = false
+    private var badClick = false
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        binding.llGoodContainer.setOnClickListener {
+            setReviewText(binding.tvGood, true)
+            setReviewText(binding.tvBad, false)
+        }
+
+        binding.llBadContainer.setOnClickListener {
+            setReviewText(binding.tvBad, true)
+            setReviewText(binding.tvGood, false)
+        }
+
+        binding.btnReviewDone.setOnClickListener {
+
+        }
     }
 
+    private fun setReviewText(textView: TextView, isFocused: Boolean) {
+        if(isFocused) {
+            textView.setTextColor(ContextCompat.getColor(requireActivity(), R.color.Primary_03))
+        } else {
+            textView.setTextColor(ContextCompat.getColor(requireActivity(), R.color.Base_05))
+        }
+    }
 }
